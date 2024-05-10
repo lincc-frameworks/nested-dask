a2a27a8376c254e67139143570994be6f57a22be
Doug Branton <brantd@uw.edu>
2024-05-10 13:36:21 -0700

    fill out unit test suite

--- tests/dask_nested/conftest.py
+++ tests/dask_nested/conftest.py
@@ -30,6 +30,36 @@ def test_dataset():
 
 
 @pytest.fixture
+def test_dataset_with_nans():
+    """stop before add_nested"""
+    n_base = 50
+    layer_size = 500
+    randomstate = np.random.RandomState(seed=1)
+
+    # Generate base data
+    a = randomstate.random(n_base)
+    a[10] = np.nan  # add a nan
+    base_data = {"a": a, "b": randomstate.random(n_base) * 2}
+    base_nf = npd.NestedFrame(data=base_data)
+
+    t = randomstate.random(layer_size * n_base) * 20
+    t[50] = np.nan  # add a nan
+
+    layer_data = {
+        "t": t,
+        "flux": randomstate.random(layer_size * n_base) * 100,
+        "band": randomstate.choice(["r", "g"], size=layer_size * n_base),
+        "index": np.arange(layer_size * n_base) % n_base,
+    }
+    layer_nf = npd.NestedFrame(data=layer_data).set_index("index")
+
+    base_dn = dn.NestedFrame.from_nestedpandas(base_nf, npartitions=5)
+    layer_dn = dn.NestedFrame.from_nestedpandas(layer_nf, npartitions=10)
+
+    return base_dn.add_nested(layer_dn, "nested")
+
+
+@pytest.fixture
 def test_dataset_no_add_nested():
     """stop before add_nested"""
     n_base = 50
--- tests/dask_nested/test_nestedframe.py
+++ tests/dask_nested/test_nestedframe.py
@@ -1,20 +1,27 @@
 import dask_nested as dn
+import numpy as np
+import pytest
 from nested_pandas.series.dtype import NestedDtype
 
 
 def test_nestedframe_construction(test_dataset):
     """test the construction of a nestedframe"""
-    pass
+    assert len(test_dataset) == 50
+    assert test_dataset.columns.to_list() == ["a", "b", "nested"]
+    assert isinstance(test_dataset["nested"].dtype, NestedDtype)
 
 
 def test_all_columns(test_dataset):
     """all_columns property test"""
-    pass
+    all_cols = test_dataset.all_columns
+
+    assert all_cols["base"].to_list() == test_dataset.columns.to_list()
+    assert all_cols["nested"] == ["t", "flux", "band"]
 
 
 def test_nested_columns(test_dataset):
     """nested_columns property test"""
-    pass
+    assert test_dataset.nested_columns == ["nested"]
 
 
 def test_add_nested(test_dataset_no_add_nested):
@@ -36,16 +43,59 @@ def test_add_nested(test_dataset_no_add_nested):
     assert len(base_with_nested.compute()) == 50
 
 
-def test_query(test_dataset):
-    """test the query function"""
-    pass
+def test_query_on_base(test_dataset):
+    """test the query function on base columns"""
+
+    # Try a few basic queries
+    assert len(test_dataset.query("a  > 0.5").compute()) == 22
+    assert len(test_dataset.query("a  > 0.5 & b > 1").compute()) == 13
+    assert len(test_dataset.query("a  > 2").compute()) == 0
 
 
-def test_dropna(test_dataset):
+def test_query_on_nested(test_dataset):
+    """test the query function on nested columns"""
+
+    # Try a few nested queries
+    res = test_dataset.query("nested.flux>75").compute()
+    assert len(res.loc[1]["nested"]) == 127
+
+    res = test_dataset.query("nested.band == 'g'").compute()
+
+    assert len(res.loc[1]["nested"]) == 232
+    assert len(res) == 50  # make sure the base df remains unchanged
+
+
+def test_dropna(test_dataset_with_nans):
     """test the dropna function"""
-    pass
+
+    nan_free_base = test_dataset_with_nans.dropna(subset=["a"])
+    # should just remove one row
+    assert len(nan_free_base) == len(test_dataset_with_nans) - 1
+
+    meta = test_dataset_with_nans.loc[0].head(0).nested.nest.to_flat()
+
+    nan_free_nested = test_dataset_with_nans.dropna(subset=["nested.t"])
+
+    # import pdb;pdb.set_trace()
+    flat_nested_nan_free = nan_free_nested.map_partitions(lambda x: x.nested.nest.to_flat(), meta=meta)
+    flat_nested = test_dataset_with_nans.map_partitions(lambda x: x.nested.nest.to_flat(), meta=meta)
+    # should just remove one row
+    assert len(flat_nested_nan_free) == len(flat_nested) - 1
 
 
 def test_reduce(test_dataset):
     """test the reduce function"""
-    pass
+
+    def reflect_inputs(*args):
+        return args
+
+    res = test_dataset.reduce(reflect_inputs, "a", "nested.t", meta=("inputs", float))
+
+    assert len(res) == 50
+    assert isinstance(res.compute().loc[0][0], float)
+    assert isinstance(res.compute().loc[0][1], np.ndarray)
+
+    res2 = test_dataset.reduce(np.mean, "nested.flux", meta=("mean", float))
+
+    assert pytest.approx(res2.compute()[15], 0.1) == 53.635174
+    assert pytest.approx(sum(res2.compute()), 0.1) == 2488.960119
